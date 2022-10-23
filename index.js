ab63c5721c6135df978beab1fad2560a58d36f0f
Ivanna Nouel <Ivanna.Nouel@itksquare.edu.mx>
2022-10-23 08:16:28 -0500

    fix

--- index.js
+++ index.js
@@ -8,14 +8,17 @@ const title = document.querySelectorAll('#title');
 
 
 
+//API on variable so is easier to pass the URL to the fetch 
 const url ='https://gist.githubusercontent.com/carmandomx/b27e23332eda1d061feb3cdada26afc0/raw/438d33407442d2abbf605e87336f48a83ccff3f5/data.json';
 
 
-
+//function to pass to button listener to fetch information from API and shows it on the web
+//Daily function
 function daily(){
     fetch(url)
     .then((response) => response.json())
     .then(data => {
+        //this forEach to pass through every item on the array 
         data.forEach((element, i) => {
             title[i].innerHTML = data[i].title 
             hours[i].innerHTML = data[i].timeframes.daily.current + 'hrs'
@@ -26,14 +29,14 @@ function daily(){
     .catch(error => console.error(error));
 
 }
-
-
-
+//Daily Weekly
 function weekly(){
     fetch(url)
     .then((response) => response.json())
     .then(data => {
+        //this forEach to pass through every item on the array 
         data.forEach((element, i) => {
+            title[i].innerHTML = data[i].title 
             hours[i].innerHTML = data[i].timeframes.weekly.current + 'hrs'
             time[i].innerHTML = 'Last Weekly - ' + data[i].timeframes.weekly.previous + 'hrs'
         })
@@ -41,12 +44,14 @@ function weekly(){
     .catch(error => console.error(error));
 
 }
-
+//Daily Monthly
 function monthly(){
     fetch(url)
     .then((response) => response.json())
     .then(data => {
+        //this forEach to pass through every item on the array 
         data.forEach((element, i) => {
+            title[i].innerHTML = data[i].title 
             hours[i].innerHTML = data[i].timeframes.monthly.current + 'hrs'
             time[i].innerHTML = 'Last Monthly - ' + data[i].timeframes.monthly.previous + 'hrs'
         })
@@ -55,7 +60,7 @@ function monthly(){
 
 }
 
-
+//btn Daily
 btndaily.addEventListener('click', () => {
     btndaily.classList.add('active')
     btnweekly.classList.remove('active')
@@ -64,20 +69,20 @@ btndaily.addEventListener('click', () => {
     daily()
 })
 
+//btn Weekly
 btnweekly.addEventListener('click', () => {
     btnweekly.classList.add('active')
     btndaily.classList.remove('active')
     btnmonthly.classList.remove('active')
 
-
     weekly()
 })
 
+//btn Monthly
 btnmonthly.addEventListener('click', () => {
     btnmonthly.classList.add('active')
     btnweekly.classList.remove('active')
     btndaily.classList.remove('active')
-
 
     monthly()
 })
